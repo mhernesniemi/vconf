0bd2d0c6639133cff718aa58acba3cd4631e7ec6
vagrant <vagrant@sanoma.dev>
2016-08-30 19:49:51 +0000

    pimp

--- config.user.php
+++ config.user.php
@@ -11,7 +11,7 @@
 <?php if(realpath(__FILE__)===realpath($_SERVER["SCRIPT_FILENAME"])){header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found');die();}?>
 {
 	"globals": {
-		"_remove_me_to_set_AUTH_LOG_FILE_COUNT"         : 100,
+		"AUTH_LOG_FILE_COUNT"         : 0,
 		"_remove_me_to_set_AUTO_UPGRADE"                : false,
 		"_remove_me_to_set_CHECK_UPGRADE"               : true,
 		"_remove_me_to_set_EXPORT"                      : true,
@@ -54,8 +54,8 @@
 			"alert"       : "danger",
 			"emerg"       : "danger",
 			"Notice"      : "info",
-			"fatal error" : "danger",
-			"parse error" : "danger",
+			"Fatal error" : "danger",
+			"Parse error" : "danger",
 			"Warning"     : "warning"
 		},
 		"http": {
@@ -109,33 +109,26 @@
 			}
 		},
 		"apache1": {
-			"display" : "Apache Error #1",
-			"path"    : "\/var\/log\/apache2\/error.log",
-			"refresh" : 5,
-			"max"     : 10,
-			"notify"  : true,
-			"format"  : {
-				"type"         : "HTTPD 2.4",
-				"regex"        : "|^\\[(.*) (.*) (.*) (.*):(.*):(.*)\\.(.*) (.*)\\] \\[(.*):(.*)\\] \\[pid (.*)\\] .*\\[client (.*):(.*)\\] (.*)(, referer: (.*))*$|U",
+			"display"   : "Apache Error #1",
+			"path"      : "\/var\/log\/apache2\/error.log",
+			"refresh"   : 5,
+			"max"       : 10,
+			"notify"    : true,
+			"multiline" : "",
+			"format"    : {
+				"regex"        : "|^\\[(.*)\\] \\[(.*)\\] (\\[client (.*)\\] )*((?!\\[client ).*)(, referer: (.*))*$|U",
 				"export_title" : "Log",
 				"match"        : {
-					"Date"    : {
-						"M" : 2,
-						"d" : 3,
-						"H" : 4,
-						"i" : 5,
-						"s" : 6,
-						"Y" : 8
-					},
-					"IP"       : 12,
-					"Log"      : 14,
-					"Severity" : 10,
-					"Referer"  : 16
+					"Date"     : 1,
+					"IP"       : 4,
+					"Log"      : 5,
+					"Severity" : 2,
+					"Referer"  : 7
 				},
 				"types": {
 					"Date"     : "date:H:i:s",
 					"IP"       : "ip:http",
-					"Log"      : "preformatted",
+					"Log"      : "pre",
 					"Severity" : "badge:severity",
 					"Referer"  : "link"
 				},
@@ -145,26 +138,26 @@
 			}
 		},
 		"apache2": {
-			"display" : "Apache Access #2",
-			"path"    : "\/var\/log\/apache2\/access.log",
-			"refresh" : 0,
-			"max"     : 10,
-			"notify"  : false,
-			"format"  : {
-				"type"         : "NCSA",
-				"regex"        : "|^((\\S*) )*(\\S*) (\\S*) (\\S*) \\[(.*)\\] \"(\\S*) (.*) (\\S*)\" ([0-9]*) (.*)( \"(.*)\" \"(.*)\"( [0-9]*/([0-9]*))*)*$|U",
+			"display"   : "Apache Access #2",
+			"path"      : "\/var\/log\/apache2\/access.log",
+			"refresh"   : 0,
+			"max"       : 10,
+			"notify"    : false,
+			"multiline" : "",
+			"format"    : {
+				"regex"        : " |^(.*) (.*) (.*) \\[(.*)\\] \"(.*) (.*) (.*)\" ([0-9]*) (.*) \"(.*)\" \"(.*)\"( [0-9]*\/([0-9]*))*$|U",
 				"export_title" : "URL",
 				"match"        : {
-					"Date"    : 6,
-					"IP"      : 3,
-					"CMD"     : 7,
-					"URL"     : 8,
-					"Code"    : 10,
-					"Size"    : 11,
-					"Referer" : 13,
-					"UA"      : 14,
-					"User"    : 5,
-					"\u03bcs" : 16
+					"Date"    : 4,
+					"IP"      : 1,
+					"CMD"     : 5,
+					"URL"     : 6,
+					"Code"    : 8,
+					"Size"    : 9,
+					"Referer" : 10,
+					"UA"      : 11,
+					"User"    : 3,
+					"\u03bcs" : 13
 				},
 				"types": {
 					"Date"    : "date:H:i:s",
